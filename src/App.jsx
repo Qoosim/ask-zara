a12ea3e1f934f15f77e505d554c301179f1a060c
Qoosim <ayinde.abdulghaniyy@gmail.com>
2025-03-08 16:13:00 +0100

    create a component to text functionality of tailwindcss install

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { Headers } from "./components/headers/Headers";
+
 export const App = () => {
   return (
     <>
-      <div className="text-5xl text-red-300 font-extrabold underline text-center pt-5">
-        Assalaam Alaykum
-      </div>
+      <Headers />
     </>
   );
 };
---
+++ src/components/headers/Headers.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Headers = () => {
+  return (
+    <>
+      <div className="text-4xl text-red-300 font-normal text-center pt-5">
+        This is the navbar section
+      </div>
+    </>
+  );
+};
