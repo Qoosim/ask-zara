706c17b533c9dfbd4e8955854b4ba5017233ba4e
Qoosim <ayinde.abdulghaniyy@gmail.com>
2025-04-09 17:02:17 +0100

    design and develop journey begins section

--- src/components/home/_components/BeginsHereComponent.jsx
+++ src/components/home/_components/BeginsHereComponent.jsx
@@ -10,15 +10,30 @@ export const BeginsHereComponent = () => {
           alt="Login Image"
           className="w-full h-full object-cover"
         />
-        <h2>Your Journey Begins Here</h2>
-        <p>
-          Create an account or log in to get personalized support, save your
-          favorite resources, and keep track of your progress. At Ask Zara,
-          we’re ready to be your partner in well-being.
-        </p>
-        <div>
-          <button>Login</button>
-          <button>Sign Up</button>
+
+        <div className="flex flex-col items-center gap-[2rem] w-[37%] 2xl:w-[27%] mx-auto absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <h2 className="text-[#000] text-[1.5rem] font-lato-bold">
+            Your Journey Begins Here
+          </h2>
+          <p className="text-center text-[0.9rem] font-montserrat text-[#000] font-normal">
+            Create an account or log in to get personalized support, save your
+            favorite resources, and keep track of your progress. At Ask Zara,
+            we’re ready to be your partner in well-being.
+          </p>
+          <div className="flex items-center gap-[2rem]">
+            <button
+              className="bg-[#fff] text-[#000] w-[9rem] py-[0.5rem] rounded-[2rem] outline-2 outline-[#ff6e60] shadow-2xl"
+              role="button"
+            >
+              Login
+            </button>
+            <button
+              className="bg-[#ff6e60] text-[#fff] w-[9rem] py-[0.5rem] rounded-[2rem] shadow-2xl"
+              role="button"
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
       </div>
     </>
